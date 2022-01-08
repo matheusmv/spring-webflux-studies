1810959c96b99d3f31eccf7d8c301e898a0c67b0
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-08 10:50:47 -0300

    project started

---
+++ src/main/java/com/github/matheusmv/springwebfluxstudies/SpringWebfluxStudiesApplication.java
@@ -0,0 +1,13 @@
+package com.github.matheusmv.springwebfluxstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxStudiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebfluxStudiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/matheusmv/springwebfluxstudies/SpringWebfluxStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.matheusmv.springwebfluxstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebfluxStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
