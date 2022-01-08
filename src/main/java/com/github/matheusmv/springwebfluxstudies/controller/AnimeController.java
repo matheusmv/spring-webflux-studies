dc33f94cf95d793b7ccce9213af05527c8df0ac3
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-08 16:06:57 -0300

    POST

--- src/main/java/com/github/matheusmv/springwebfluxstudies/controller/AnimeController.java
+++ src/main/java/com/github/matheusmv/springwebfluxstudies/controller/AnimeController.java
@@ -3,12 +3,18 @@ package com.github.matheusmv.springwebfluxstudies.controller;
 import com.github.matheusmv.springwebfluxstudies.domain.Anime;
 import com.github.matheusmv.springwebfluxstudies.service.AnimeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/animes")
@@ -25,5 +31,11 @@ public class AnimeController {
     @GetMapping("/{id}")
     public Mono<Anime> findById(@PathVariable Long id) {
         return animeService.findById(id);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Anime> save(@Valid @RequestBody Anime anime) {
+        return animeService.save(anime);
     }
 }
