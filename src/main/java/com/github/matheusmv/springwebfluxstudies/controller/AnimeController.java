5a6702fd5364a10c6624d73cf5608e4706cca3f1
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-08 16:13:23 -0300

    PUT

--- src/main/java/com/github/matheusmv/springwebfluxstudies/controller/AnimeController.java
+++ src/main/java/com/github/matheusmv/springwebfluxstudies/controller/AnimeController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -37,5 +38,11 @@ public class AnimeController {
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<Anime> save(@Valid @RequestBody Anime anime) {
         return animeService.save(anime);
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public Mono<Void> update(@Valid @RequestBody Anime anime) {
+        return animeService.update(anime);
     }
 }
