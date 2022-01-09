d03563f0e1db3907e9bd848fd2dcc551673d80b0
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-09 11:55:03 -0300

    test controller listAll

---
+++ src/test/java/com/github/matheusmv/springwebfluxstudies/controller/AnimeControllerTest.java
@@ -0,0 +1,71 @@
+package com.github.matheusmv.springwebfluxstudies.controller;
+
+import com.github.matheusmv.springwebfluxstudies.domain.Anime;
+import com.github.matheusmv.springwebfluxstudies.service.AnimeService;
+import com.github.matheusmv.springwebfluxstudies.util.AnimeCreator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import reactor.blockhound.BlockHound;
+import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
+
+import java.util.concurrent.FutureTask;
+import java.util.concurrent.TimeUnit;
+
+@ExtendWith(SpringExtension.class)
+public class AnimeControllerTest {
+
+    @Mock
+    private AnimeService animeService;
+
+    @InjectMocks
+    private AnimeController animeController;
+
+    private final Anime anime = AnimeCreator.createValidAnime();
+
+    @BeforeAll
+    public static void blockHoundSetUp() {
+        BlockHound.install();
+    }
+
+    @Test
+    public void blockHoundWorks() {
+        try {
+            var task = new FutureTask<>(() -> {
+                Thread.sleep(0);
+                return "";
+            });
+            Schedulers.parallel().schedule(task);
+
+            task.get(10, TimeUnit.SECONDS);
+            Assertions.fail("should fail");
+        } catch (Exception e) {
+            Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
+        }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        BDDMockito.when(animeService.findAll())
+                .thenReturn(Flux.just(anime));
+    }
+
+    @Test
+    @DisplayName("findAll returns a flux of Anime")
+    public void findAllReturnFluxOfAnime_WhenSuccessful() {
+        StepVerifier.create(animeController.listAll())
+                .expectSubscription()
+                .expectNext(anime)
+                .verifyComplete();
+    }
+}
