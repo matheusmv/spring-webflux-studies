0d912a367be26124d9ef9a41066eca1d34f61161
Matheus Matias <matheus.matias.viana@hotmail.com>
2022-01-09 11:09:28 -0300

    test service findAll

--- src/test/java/com/github/matheusmv/springwebfluxstudies/service/AnimeServiceTest.java
+++ src/test/java/com/github/matheusmv/springwebfluxstudies/service/AnimeServiceTest.java
@@ -5,14 +5,19 @@ import com.github.matheusmv.springwebfluxstudies.repository.AnimeRepository;
 import com.github.matheusmv.springwebfluxstudies.util.AnimeCreator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import reactor.blockhound.BlockHound;
 import reactor.blockhound.BlockingOperationError;
+import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
+import reactor.test.StepVerifier;
 
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.TimeUnit;
@@ -47,5 +52,20 @@ public class AnimeServiceTest {
         } catch (Exception e) {
             Assertions.assertTrue(e.getCause() instanceof BlockingOperationError);
         }
+    }
+
+    @BeforeEach
+    public void setUp() {
+        BDDMockito.when(animeRepository.findAll())
+                .thenReturn(Flux.just(anime));
+    }
+
+    @Test
+    @DisplayName("findAll returns a flux of Anime")
+    public void findAllReturnFluxOfAnime_WhenSuccessful() {
+        StepVerifier.create(animeService.findAll())
+                .expectSubscription()
+                .expectNext(anime)
+                .verifyComplete();
     }
 }
